6e216687c4d9671853fe36f2b714f9092a333534
Vaibhav  Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-04-13 00:18:55 +0530

    printing the binary tree in spiral form

--- BinaryTree/spiralForm.cpp
+++ BinaryTree/spiralForm.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include<queue>
+#include <queue>
 #include <stack>
 #include "BinaryTreeNode.h"
 using namespace std;
 
 // method:-1
+// time:- O(n)
 void spiralForm(BinaryTreeNode<int>* root){
 
 	queue<BinaryTreeNode<int>*> q;
@@ -38,7 +39,39 @@ void spiralForm(BinaryTreeNode<int>* root){
 	}
 }
 
+// method:-2 efficient (using only one stack)
+// time:- O(n)
+void spiralForm2(BinaryTreeNode<int>* root){
+	stack<BinaryTreeNode<int>*> s1;
+	stack<BinaryTreeNode<int>*> s2;
+	s1.push(root);
+	while(s1.size() != 0 || s2.size() != 0){
+		while(s1.size() != 0){
+			BinaryTreeNode<int> * top = s1.top();
+			cout << top ->data << ",";
+			s1.pop();
+			if(top -> left != NULL)
+				s2.push(top -> left);
+			if(top -> right != NULL)
+				s2.push(top -> right);
+		}
+		cout << endl;
+		while(s2.size() != 0){
+			BinaryTreeNode<int> * top = s2.top();
+			cout << top -> data << ",";
+			s2.pop();
+			if(top -> right != NULL)
+				s1.push(top -> right);
+			if(top -> left != NULL)
+				s1.push(top -> left);
+		}
+		cout << endl;
+	}
+}
+
 int main(){
 	BinaryTreeNode<int>* root = takeInput2();
 	spiralForm(root);
+	cout << endl;
+	spiralForm2(root);
 }
